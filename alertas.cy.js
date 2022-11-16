bbc70bad9064679bd5b57489f9c7393346c9a2b4
AmonraReis <75175885+AmonraReis@users.noreply.github.com>
2022-11-16 20:19:53 -0300

    Update alertas.cy.js

--- alertas.cy.js
+++ alertas.cy.js
@@ -1,25 +1,45 @@
 describe("Alertas", () => {
- 
-    beforeEach(() => {
-      cy.visit('https://automacaocombatista.herokuapp.com/mudancadefoco/alert');
 
-    })
+	//beforeEach(() => {
+	//cy.visit('https://automacaocombatista.herokuapp.com/mudancadefoco/alert');
 
-    it('Digitar em uma prompt js', () => {
+	//})
 
-      
+	it('Digitar em uma prompt js', () => {
 
-      cy.window().then(function(p){
-        //stubbing prompt window
-        cy.stub(p, "prompt").returns("teste-prompt-digitado");
-        // click on Click for JS Prompt button
-        cy.get(':nth-child(7) > .btn').click()
-        // verify application message on clicking on OK
-        cy.get('#result').contains('teste-prompt-digitado')
+		cy.visit('https://automacaocombatista.herokuapp.com/mudancadefoco/alert');
+
+		cy.window().then(function(p) {
+			//stubbing prompt window
+			cy.stub(p, "prompt").returns("teste-prompt-digitado");
+			// click on Click for JS Prompt button
+			cy.get(':nth-child(7) > .btn').click()
+			// verify application message on clicking on OK
+			cy.get('#result').contains('teste-prompt-digitado')
 
 
-       
-        
-    })
- });
- });
+
+		})
+
+
+	});
+	it('Janela filha', () => {
+
+		cy.visit('https://automacaocombatista.herokuapp.com/mudancadefoco/janela');
+
+		cy.get('.btn')
+		.invoke('removeAttr', 'target').click()
+
+
+		cy.url().should('include', 'https://automacaocombatista.herokuapp.com/mudancadefoco/newwindow')
+		cy.contains('Você Abriu uma nova janela!!')
+    cy.go('back');
+
+
+
+
+		
+
+
+	});
+});
